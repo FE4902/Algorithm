8f9a5248454266723aa715f952cb088378a00dc6
FE4902 <isdn000123@gmail.com>
2023-02-24 21:34:19 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,5 +1,3 @@
 function solution(age) {
-    const abcArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
-    
-    return [...age.toString()].map(v => abcArray[v]).join('');
+    return [...age.toString()].map(v => 'abcdefghij'[v]).join('');
 }
