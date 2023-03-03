2b19624fe04707813446c4d118bc44dbb979814a
FE4902 <isdn000123@gmail.com>
2023-03-04 08:19:16 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,6 +1,3 @@
 function solution(s) {
-    const lower = [...s].filter(v => v === v.toLowerCase()).sort((a, b) => b.localeCompare(a));
-    const upper = [...s].filter(v => v === v.toUpperCase()).sort((a, b) => b.localeCompare(a));
-    
-    return [...lower, ...upper].join('');
+    return [...s].sort().reverse().join('');
 }
