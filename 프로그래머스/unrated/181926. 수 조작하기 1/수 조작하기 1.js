f3aff2f708e50d99104b083f7ef140226a33ef1e
FE4902 <isdn000123@gmail.com>
2023-06-14 22:48:42 +0900

    [unrated] Title: 수 조작하기 1, Time: 3.63 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,11 @@
+function solution(n, control) {
+    for (const v of control) {
+        switch (v) {
+            case 'w': n = n + 1; break;
+            case 's': n = n - 1; break;
+            case 'd': n = n + 10; break;
+            case 'a': n = n - 10; break;
+        }
+    }
+    return n;
+}
