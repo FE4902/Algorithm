956f191120f2445004489e6d0a8d94b044686e97
FE4902 <isdn000123@gmail.com>
2023-09-18 23:08:35 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,13 @@
+function solution(a, b, c) {
+    let answer = a + b + c;
+    
+    if (a === b || b === c || a === c){
+        answer *= (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2))
+    }
+    
+    if(a === b && b === c && a === c){
+        answer *= (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3))
+    }
+    
+    return answer;
+}
