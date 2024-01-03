f2a3162d41b557d76fc70aec927c589f077bca2c
FE4902 <isdn000123@gmail.com>
2024-01-03 22:12:34 +0900

    [level unrated] Title: 수열과 구간 쿼리 4, Time: 4.12 ms, Memory: 35.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -0,0 +1,10 @@
+function solution(arr, queries) {
+    queries.forEach(v => {
+        for(let i = v[0]; i <= v[1]; i++){
+            if(i % v[2] === 0) {
+                arr[i] += 1;
+            }
+        }
+    })
+    return arr;
+}
