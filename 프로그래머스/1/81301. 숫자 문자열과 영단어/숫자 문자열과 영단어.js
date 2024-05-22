b4102fd9afda1fe359e11612bcf4bdd57215ae4a
FE4902 <isdn000123@gmail.com>
2024-05-22 20:53:14 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,20 @@
+const solution = (s) => {
+    const obj = {
+        'zero': 0,
+        'one': 1,
+        'two': 2,
+        'three': 3,
+        'four': 4,
+        'five': 5,
+        'six': 6,
+        'seven': 7,
+        'eight': 8,
+        'nine': 9,
+    }
+    
+    for(let i of Object.entries(obj)){
+        s = s.replaceAll(i[0], i[1])
+    }
+
+    return Number(s);
+}
